003165608869db1ec8287756db01d79349670d0c
Hesham <mohamed1.hesham22@gmail.com>
2021-10-14 22:39:49 +0200

    lets test

--- lib/models/menu_group_model.dart
+++ lib/models/menu_group_model.dart
@@ -3,6 +3,7 @@ import 'package:shop_box/models/sellableItem_model.dart';
 // menuGroup Rules
 // must contains at least 5 items 3 different items
 // menuGroup must be at least 30% percentage
+// menuGroup must be at least 30% percentage
 class MenuGroup {
   final String menuPercentage;
   final int quantity;
